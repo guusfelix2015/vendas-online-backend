a2747a8b6c8ddac2816e08e0b84c6e29eb2a37a2
guusfelix2015 <gusfelix2015@gmail.com>
2023-07-12 06:58:13 -0300

    cache module generico

--- src/city/city.module.ts
+++ src/city/city.module.ts
@@ -3,14 +3,16 @@ import { CityController } from './city.controller';
 import { CityService } from './city.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CityEntity } from './entities/city.entity';
-import { CacheModule } from '@nestjs/cache-manager';
+import { CacheModule as CacheModuleNest } from '@nestjs/cache-manager';
+import { CacheModule } from '../cache/cache.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([CityEntity]),
-    CacheModule.register({
+    CacheModuleNest.register({
       ttl: 60 * 60 * 24,
     }),
+    CacheModule,
   ],
   controllers: [CityController],
   providers: [CityService],
