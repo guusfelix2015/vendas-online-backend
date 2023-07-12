7fb1ab6bb41536870d93ef9e5460f96d0d7f7164
guusfelix2015 <gusfelix2015@gmail.com>
2023-07-12 08:29:19 -0300

    tratamentos de exceção

--- src/address/address.service.ts
+++ src/address/address.service.ts
@@ -3,19 +3,24 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { AddressEntity } from './entities/address.entity';
 import { Repository } from 'typeorm';
 import { CreateAddressDto } from './dtos/createAddress.dto';
+import { UserService } from 'src/user/user.service';
+import { CityService } from 'src/city/city.service';
 
 @Injectable()
 export class AddressService {
   constructor(
     @InjectRepository(AddressEntity)
     private readonly addressRepository: Repository<AddressEntity>,
+    private readonly userService: UserService,
+    private readonly cityService: CityService,
   ) {}
 
   async createAddress(
     createAddress: CreateAddressDto,
     userId: number,
   ): Promise<AddressEntity> {
-    console.log('createAddress', createAddress);
+    await this.userService.findUserById(userId);
+    await this.cityService.findCityById(createAddress.cityId);
     return await this.addressRepository.save({
       ...createAddress,
       userId,
--- src/city/city.service.ts
+++ src/city/city.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CityEntity } from './entities/city.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -16,5 +16,17 @@ export class CityService {
     return this.cacheService.getCache<CityEntity[]>(`state_${stateId}`, () =>
       this.cityRepository.find({ where: { stateId } }),
     );
+  }
+
+  async findCityById(cityId: number): Promise<CityEntity> {
+    const city = await this.cityRepository.findOne({
+      where: { id: cityId },
+    });
+
+    if (!city) {
+      throw new NotFoundException('Citie id not found');
+    }
+
+    return city;
   }
 }
