a76634e2de20869bc6c43015a2918faac1442bef
guusfelix2015 <gusfelix2015@gmail.com>
2023-07-12 10:58:59 -0300

    create module auth

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -51,4 +51,16 @@ export class UserService {
 
     return user;
   }
+
+  async findUserByEmail(email: string): Promise<UserEntity> {
+    const user = await this.userRepository.findOne({
+      where: { email },
+    });
+
+    if (!user) {
+      throw new NotFoundException('User email not found');
+    }
+
+    return user;
+  }
 }
