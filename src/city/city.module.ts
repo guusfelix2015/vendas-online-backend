16e060f6c063206fbf9727f2e50497694f834697
guusfelix2015 <gusfelix2015@gmail.com>
2023-07-12 06:59:19 -0300

    removendo cache module do modulo de city

--- src/city/city.module.ts
+++ src/city/city.module.ts
@@ -3,17 +3,10 @@ import { CityController } from './city.controller';
 import { CityService } from './city.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CityEntity } from './entities/city.entity';
-import { CacheModule as CacheModuleNest } from '@nestjs/cache-manager';
 import { CacheModule } from '../cache/cache.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([CityEntity]),
-    CacheModuleNest.register({
-      ttl: 60 * 60 * 24,
-    }),
-    CacheModule,
-  ],
+  imports: [TypeOrmModule.forFeature([CityEntity]), CacheModule],
   controllers: [CityController],
   providers: [CityService],
 })
