bb7ed5983d63c46bda9b173bf852a878c50f917f
guusfelix2015 <gusfelix2015@gmail.com>
2023-07-13 08:20:37 -0300

    criação de decorator customizado

--- src/address/address.controller.ts
+++ src/address/address.controller.ts
@@ -3,26 +3,27 @@ import {
   Post,
   UsePipes,
   ValidationPipe,
-  Param,
   Body,
 } from '@nestjs/common';
 import { CreateAddressDto } from './dtos/createAddress.dto';
 import { AddressService } from './address.service';
 import { AddressEntity } from './entities/address.entity';
+import { UserId } from 'src/decorators/user-id.decorator';
 import { Roles } from 'src/decorators/roles.decorator';
 import { UserType } from 'src/user/enum/user-type.enum';
 
+@Roles(UserType.User)
 @Controller('address')
 export class AddressController {
   constructor(private readonly addressService: AddressService) {}
 
-  @Roles(UserType.User)
-  @Post('/:userId')
+  @Post()
   @UsePipes(ValidationPipe)
   async createAddress(
-    @Param('userId') userId: number,
     @Body() createAddressDto: CreateAddressDto,
+    @UserId() userId: number,
   ): Promise<AddressEntity> {
+    console.log(userId);
     return await this.addressService.createAddress(createAddressDto, userId);
   }
 }
